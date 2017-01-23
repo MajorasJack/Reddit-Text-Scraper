8b85e701babfe26bb26c2c73722e24d359ec5bff
Jack Evans <jack.evans@cirrus-connect.com>
2017-01-23 23:07:50 +0000

    Pushing project

---
+++ reddit-scraper.js
@@ -0,0 +1,22 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var request = require('request');
+var fs = require('fs');
+
+var subReddit = "ShowerThoughts";
+
+request("https://www.reddit.com/r/" + subReddit + "/top/?limit=100&t=month&sort=top&count=0", function(error, response, body) {
+  if(error) {
+    console.log("Error: " + error);
+  }
+  console.log("Status code: " + response.statusCode);
+
+  var $ = cheerio.load(body);
+
+
+$('div#siteTable > div.link').each(function( index ) {
+    var title = $(this).find('p.title > a.title').text().trim();
+    fs.appendFileSync(subReddit + '-reddit.txt', title + '\n');
+  });
+
+});
